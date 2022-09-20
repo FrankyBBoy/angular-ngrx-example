c71e0af312e3d0659c4c06baf3aefc35b9276b83
FrankyBBoy <francis1592@gmail.com>
2022-09-20 09:40:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-gtvytn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-gtvytn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {}
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
